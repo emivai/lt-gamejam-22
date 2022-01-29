2d3442382780934983ea827ab3a53266af9e111c
LRemp <45431254+LRemp@users.noreply.github.com>
2022-01-29 16:36:06 +0200

    follow camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float smoothness;
+    public Transform targetObject;
+    private Vector3 initalOffset;
+    private Vector3 cameraPosition;
+
+    void Start()
+    {
+        initalOffset = transform.position - targetObject.position;
+    }
+
+    void FixedUpdate()
+    {
+        cameraPosition = targetObject.position + initalOffset;
+        transform.position = Vector3.Lerp(transform.position + new Vector3(0.0f, 0.025f, 0.0f), cameraPosition, smoothness * Time.fixedDeltaTime);
+    }
+}
