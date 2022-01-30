49c93a48902d3eb4a63437b0a0e4623aec6e81a6
Domantas231 <domantas231@gmail.com>
2022-01-30 03:44:37 +0200

    added some not working checking if a bullet has left the screen

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,18 @@ public class Bullet : MonoBehaviour
 {
     public Rigidbody2D rb;
     public int dmg = 1;
+    public float extraArea = 10f;
+
+    void Update(){
+        /*
+         * TODO: Fix this fucking shit
+         */
+        if(Camera.main.transform.position.x + extraArea - gameObject.transform.position.x < 0 &&
+           Camera.main.transform.position.y + extraArea - gameObject.transform.position.y < 0){
+               Destroy(gameObject);
+           }
+
+    }
 
     void OnTriggerEnter2D(Collider2D hitInfo){
         if(hitInfo.name == "Player"){
